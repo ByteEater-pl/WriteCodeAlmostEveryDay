676d5bb6ab3ef169169fdbe76d78ab0a04df6a33
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2020-04-07 23:35:32 +0200

    Correct off-by-one error

--- Haskell/SelectRandomPairs/select.hs
+++ Haskell/SelectRandomPairs/select.hs
@@ -2,6 +2,6 @@ import System.Random
 main = select "abcdefgh" 15 >>= print
 
 select l n = do
-  ix@(_:t) <- randomRs (0, length l) <$> newStdGen
+  ix@(_:t) <- randomRs (0, length l - 1) <$> newStdGen
   pure $ take n $
   [(l!!i,l!!j) | (i,j) <- zip ix t, i<j]
