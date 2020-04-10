886a1ed029cddb329eac429045806bf91aea8cbd
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2020-04-10 23:56:06 +0200

    Add 2 different implementations of Thue-Morse sequence

--- Haskell/ThueMorse/ThueMorse.hs
+++ Haskell/ThueMorse/ThueMorse.hs
@@ -1,4 +1,14 @@
 import Data.Function
-main = print $ take 9 seqThueMorse
+main = print $ take 9 <$>
+  [seqThueMorse, seqThueMorse2, seqThueMorse3]
 
 seqThueMorse = fix $ (0:).tail.(>>= \x->[x,1-x])
+
+seqThueMorse2 =
+  [iterate (\l -> l ++ ((1-) <$> l)) [0]
+    !!n!!n
+  | n<-[0..]]
+
+seqThueMorse3@s@(_:r) =
+  0 : interleave ((1-) <$> s) r where
+    interleave (h:t) b = h : interleave b t
