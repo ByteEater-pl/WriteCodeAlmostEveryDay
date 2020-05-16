a7be9ae2a4e7a4f25fad74654e4b923164612dc2
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2020-05-16 23:58:06 +0200

    Implement quicksort

---
+++ Prolog/QuickSort/quicksort.pl
@@ -0,0 +1,16 @@
+qsort([],[]).
+qsort([H|T],S) :-
+  part(H,T,L,E,G),
+  qsort(L,A),
+  qsort(G,Z),
+  append([A,[H|E],Z], S).
+part(_,[],[],[],[]).
+part(H,[X|T],[X|L],E,G) :- X<H, part(H,T,L,E,G).
+part(H,[H|T],L,[H|E],G) :- part(H,T,L,E,G).
+part(H,[X|T],L,E,[X|G]) :- X>H, part(H,T,L,E,G).
+
+main :-
+  randseq(9,22,L),
+  qsort(L,S),
+  write(L),
+  write(S).
