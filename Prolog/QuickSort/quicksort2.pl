724e5cb63862335287ff081ab5bd2e17d198c47d
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2020-05-18 00:30:09 +0200

    Compact 3 append calls and arguments using maplist

--- Prolog/QuickSort/quicksort2.pl
+++ Prolog/QuickSort/quicksort2.pl
@@ -1,18 +1,16 @@
 qsort([],[]).
 qsort([H|T],S) :-
-  part(H,T,L,E,G),
+  part(H,T,[L,E,G]),
   qsort(L,A),
   qsort(G,Z),
   append([A,[H|E],Z], S).
-part(_,[],[],[],[]).
-part(H,[X|T],L,E,G) :-
-  compare(R,X,H),
-  member([R,[X]],[[<,A],[=,B],[>,C]]),
+part(_,[],[[],[],[]]).
+part(H,[X|T],R) :-
+  compare(O,X,H),
+  member([O,[X]],[[<,A],[=,B],[>,C]]),
   append([A,B,C],[X]),
-  append(A,K,L),
-  append(B,D,E),
-  append(C,F,G),
-  part(H,T,K,D,F).
+  maplist(append,[A,B,C],I,R),
+  part(H,T,I).
 
 main :-
   length(L,22),
