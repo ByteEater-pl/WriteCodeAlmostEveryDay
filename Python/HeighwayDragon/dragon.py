8584576398c7d4317df29e4d77ced002b0d90220
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2022-03-22 23:54:36 +0100

    Correct drawing

--- Python/HeighwayDragon/dragon.py
+++ Python/HeighwayDragon/dragon.py
@@ -1,13 +1,28 @@
 import turtle
 t = turtle.Turtle()
 
-def dragon(n, a, o = 1):
-  if n > 0:
-    t.left(45 * o)
-    dragon(n - 1, a * 2 ** 0.5, o)
-    t.right(90 * o)
-    dragon(n - 1, a * 2 ** 0.5, -o)
-    t.left(45 * o)
-  else: t.forward(a)
+def dragon(n, a):
+  if n:
+    a /= 2 ** 0.5
+    t.lt(45)
+    dragon(n - 1, a)
+    t.rt(90)
+    t.pu()
+    t.fd(a)
+    t.pd()
+    t.lt(180)
+    dragon(n - 1, a)
+    t.lt(180)
+    t.pu()
+    t.fd(a)
+    t.pd()
+    t.lt(45)
+  else: t.fd(a)
 
-dragon(5, 9)
+t.ht()
+t.speed(0)
+t.getscreen().delay(0)
+t.pu()
+t.bk(150)
+t.pd()
+dragon(6, 300)
